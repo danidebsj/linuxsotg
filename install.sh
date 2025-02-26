e2f34425979e1d5f6f1c67bbc68d73e6aa4889b5
danidebsj <89673419+danidebsj@users.noreply.github.com>
2025-02-26 17:35:26 -0300

    update
    el comando sh modifique

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
-/bin/cp -a /etc/issue /etc/issue.backup_issue_brander --verbose ;
-/bin/cp -a /etc/issue /etc/issue-original --verbose;
-/bin/cat ./issue.base > /etc/issue-original;
+/bin/cp -a /etc/linuxsotg /etc/linuxsotg.backup_issue_brander --verbose ;
+/bin/cp -a /etc/linuxsotg /etc/linuxsotg-original --verbose;
+/bin/cat ./issue.base > /etc/linuxsotg-original;
 /bin/cp -v ./addip2issue /etc/network/if-up.d/addip2issue;
 /bin/chmod 755 /etc/network/if-up.d/addip2issue --verbose;
 /bin/cp ./get-ip-address /usr/local/bin/get-ip-address --verbose;
